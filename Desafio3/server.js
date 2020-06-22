24e674f0fd659ebd449206337e754e6f8174b558
Aecio Silva <aeciofcs@gmail.com>
2020-06-22 20:19:57 -0300

    Instalando nodemon e criando rota /

--- Desafio3/server.js
+++ Desafio3/server.js
@@ -1,6 +1,11 @@
 const Express = require('express');
 const Server = Express();
 
+Server.get('/', (request, response)=>{
+    response.send('Oi');    
+});
+
+
 Server.listen(5000, ()=>{
     console.log('Server is Running...')
 }); // Iniciando o Servidor.. porta 5000
