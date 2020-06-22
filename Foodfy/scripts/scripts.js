3b5548d0de11de4524eb1cdfbf96d4e836f448d3
Aecio Silva <aeciofcs@gmail.com>
2020-06-22 15:27:10 -0300

    LaunchBase: Foodfy. Finalizando modal receitas.

---
+++ Foodfy/scripts/scripts.js
@@ -0,0 +1,24 @@
+const recipes       = document.querySelectorAll('.recipe');
+const modalOverlay  = document.querySelector('.modal-overlay');
+const modal         = document.querySelector('.modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+// => Ouvir o clique de todas as receitas da pagina
+
+/* Abrir o Modal Overlay */
+for (const recipe of recipes) {
+    recipe.addEventListener('click', ()=>{
+        const imgId    = recipe.getAttribute('id');        
+        modalOverlay.classList.toggle('active');
+        modal.querySelector('img').src = `./assets/${imgId}.png`;
+        modal.querySelector('h3').innerHTML = recipe.querySelector('h3').innerHTML;
+        modal.querySelector('h4').innerHTML = recipe.querySelector('h4').innerHTML;        
+    });
+}
+
+/* Fechar o Modal Overlay */
+btnCloseModal.addEventListener('click', ()=>{
+    modalOverlay.classList.toggle('active');
+});
+
+// <= Ouvir o clique de todas as receitas da pagina
