a9321c605201cc1f4a50a6537921397e68d28c44
Aecio Silva <aeciofcs@gmail.com>
2020-06-22 22:05:46 -0300

    Trazendo o front-end para o projeto.

--- Desafio3/server.js
+++ Desafio3/server.js
@@ -1,11 +1,31 @@
-const Express = require('express');
+const Express  = require('express');
+const Nunjucks = require('nunjucks');
+
 const Server = Express();
 
-Server.get('/', (request, response)=>{
-    response.send('Oi');    
+// Usar arquivos estáticos. 
+Server.use(Express.static('public'));
+
+// Configurando Nunjucks.
+Server.set('view engine', 'html');
+Nunjucks.configure('views', {
+    express: Server
 });
 
+// Rota para / => raiz da pagina
+Server.get('/', (request, response)=>{
+    return response.render('index');    
+});
 
+Server.get('/courses', (request, response)=>{
+    return response.render('courses');
+});
+
+Server.get('/about', (request, response)=>{
+    return response.render('about');
+});
+
+//Ligando o Server, porta
 Server.listen(5000, ()=>{
     console.log('Server is Running...')
-}); // Iniciando o Servidor.. porta 5000
+});
