55535a40da04b46c8732c535d76d252ad87c8628
Aecio Silva <aeciofcs@gmail.com>
2020-06-23 09:53:47 -0300

    Desafio 3-2. Arquivos nunjucks e Dados dinamico.

--- Desafio3/server.js
+++ Desafio3/server.js
@@ -1,5 +1,6 @@
 const Express  = require('express');
 const Nunjucks = require('nunjucks');
+const Courses  = require('./data');
 
 const Server = Express();
 
@@ -9,7 +10,8 @@ Server.use(Express.static('public'));
 // Configurando Nunjucks.
 Server.set('view engine', 'njk');
 Nunjucks.configure('views', {
-    express: Server
+    express: Server,
+    autoescape: false
 });
 
 // Rota para / => raiz da pagina
@@ -18,11 +20,21 @@ Server.get('/', (request, response)=>{
 });
 
 Server.get('/courses', (request, response)=>{
-    return response.render('courses');
+    return response.render('courses', {Courses});
 });
 
 Server.get('/about', (request, response)=>{
-    return response.render('about');
+    const data = {
+        title: "Rocketseat",
+        description: 'Somos uma comunidade incrível de programadores em busca do próximo nível.</br> Por isso a Rocketseat oferece através de uma plataforma inteligente e metodologia prática, além de comunidade, eventos, conteúdo e conexão com o mercado de trabalho, todas as ferramentas que você precisa para masterizar no menor tempo possível as tecnologias mais modernas de desenvolvimento web e mobile, e dessa forma avançar para o próximo nível como programador.',
+        tecnologies: ['HTML5','CSS','JavaScript','JavaScript ES6','NodeJS','ReactJS','React Native'],
+        networks: [
+            {name: "GitHub", url: "https://github.com/rocketseat"},
+            {name: "Facebook", url: "https://facebook.com/rocketseat"},
+            {name: "Twitter", url: "https://twitter.com/rocketseat"},
+        ]
+    };
+    return response.render('about', {data});
 });
 
 // Rota para pagina não encontrada.
