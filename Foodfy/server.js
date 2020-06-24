7c01151cf0366fa8d12d8bbccc524f7668df83dc
Aecio Silva <aeciofcs@gmail.com>
2020-06-24 00:08:10 -0300

    Foodfy: ajustando rota da receita unica.

--- Foodfy/server.js
+++ Foodfy/server.js
@@ -21,8 +21,23 @@ server.get('/', (req, res)=>{
     return res.render('index', {dataRecipes})
 })
 
+
+server.get('/recipes/:index', (req,res) => {    
+    const recipeIndex = dataRecipes.findIndex( (recipe)=>{ return recipe.title === req.params.index} )
+    const recipe = dataRecipes[recipeIndex]    
+    return res.send(recipe)//res.render('recipe', {recipe} )
+})
+
 server.get('/recipes', (req,res) => {
     //console.log(dataRecipes.findIndex( (recipe)=>{ return recipe.title === 'Asinhas de frango ao barbecue'} ))
+    const receitas = [...dataRecipes]
+    const receitasIndices = receitas.reduce( (acc,receita) => {
+        acc += receita 
+        //console.log(receitas.indexOf(receita))
+    }, [] )
+
+
+
     return res.render('recipes', {dataRecipes})
 })
 
