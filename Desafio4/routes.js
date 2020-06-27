162f54510794bdc6d5c0b1aac4d54142d0121c62
Aecio Silva <aeciofcs@gmail.com>
2020-06-27 11:52:52 -0300

    Desafio4: Animando o card teachers.

--- Desafio4/routes.js
+++ Desafio4/routes.js
@@ -5,4 +5,12 @@ router.get('/', (request, response) => {
     return response.render('index')
 })
 
+router.get('/teachers', (request, response) => {
+    return response.render('teachers/index')
+})
+
+router.get('/students', (request, response) => {
+    return response.render('students/index')
+})
+
 module.exports = router
