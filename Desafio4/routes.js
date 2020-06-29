300c232eb6aa77de88c75649a9d3b7455f6a1218
Aecio Silva <aeciofcs@gmail.com>
2020-06-29 00:42:14 -0300

    Desafio4: Iniciando cad dos teachers, create form.

--- Desafio4/routes.js
+++ Desafio4/routes.js
@@ -9,6 +9,14 @@ router.get('/teachers', (request, response) => {
     return response.render('teachers/index')
 })
 
+router.get('/teachers/create', (request, response) => {
+    return response.render('teachers/create')
+})
+
+router.post('/teachers', (request, response) => {
+    return response.send('DADOS DO PROFESSOR RECEBIDO !')
+})
+
 router.get('/students', (request, response) => {
     return response.render('students/index')
 })
