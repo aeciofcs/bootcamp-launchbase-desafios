4822290a3cb3459174feec2a272e6e80c5c0f345
Aecio Silva <aeciofcs@gmail.com>
2020-06-28 23:54:59 -0300

    Modulo4: Gravando dados do instrutor no data.json

--- Modulo4/instrutores.js
+++ Modulo4/instrutores.js
@@ -1,3 +1,6 @@
+const fs   = require('fs')
+const data = require('./data.json') 
+
 exports.post = (request, response) => {
     
     // Pega todas as keys do request.body
@@ -9,6 +12,17 @@ exports.post = (request, response) => {
             return response.send('Por favor, preencha todos os campos.')
     }
     
-    return response.send(keys)
+    request.body.id         = Number(data.instrutores.length + 1)
+    request.body.birth      = Date.parse(request.body.birth)
+    request.body.created_at = Date.now()
+    
+    data.instrutores.push(request.body)
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), (err)=>{
+        if (err) return request.send("Erro na gravação do arquivo DATA.JSON")
+        return response.redirect('/instrutores')
+    })
+
+    //return response.send(keys)
     //return response.send('Informações Recebidas')
 }
