62feaa0c627d031e7c7409aba632f5efaf90dec0
Aecio Silva <aeciofcs@gmail.com>
2020-07-03 21:22:11 -0300

    Modulo4: Menu ativo.

--- Modulo4/public/scripts.js
+++ Modulo4/public/scripts.js
@@ -1,46 +1,8 @@
-const modalOverlay     = document.querySelector('.modal-overlay');
-const modal            = document.querySelector('.modal');
-const cards            = document.querySelectorAll('.card');
-const btnCloseModal    = document.querySelector('.close-modal');
-const btnMaximizeModal = document.querySelector('.maximize-modal');
-const btnMinimizeModal = document.querySelector('.minimize-modal');
+const currentPage = location.pathname
+const menuItems   = document.querySelectorAll("header .links a")
 
-/* Abrir/Fechar o Modal -> */
-for (const card of cards) {
-    card.addEventListener('click', function(){
-        const pageId = card.getAttribute('id');
-        // window.location.href = `/course?id=${pageId}`
-        window.location.href = `/course/${pageId}`
-        //modalOverlay.classList.toggle('active');
-        //modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${pageId}`;
-    });
+for (const item of menuItems) {
+    if (currentPage.includes(item.getAttribute("href"))) {
+        item.classList.add("active")
+    }
 }
-
-/*
-// Posso também, não declarar uma variavel para o close do Modal,
-//pois não vamos utilizar para nada. Somente para clicar no botão.
-//Então podemos colocar direto: 
-//document.querySelector('.close-modal').addEventListiner('click', ()=>{} ) 
-
-btnCloseModal.addEventListener('click',function(){
-    modalOverlay.classList.toggle('active');
-    modalOverlay.querySelector('iframe').src = "";
-});
-
-// <- Abrir/Fechar o Modal 
-
-
-// Maximizar e Minimizar o Modal -> 
-btnMaximizeModal.addEventListener('click', function(){
-    modal.classList.toggle('maximized');
-    btnMinimizeModal.classList.toggle('active');
-    btnMaximizeModal.classList.toggle('active');   
-});
-
-btnMinimizeModal.addEventListener('click', function(){
-    btnMaximizeModal.classList.toggle('active');
-    btnMinimizeModal.classList.toggle('active');    
-    modal.classList.toggle('maximized');
-});
-// <- Maximizar e Minimizar o Modal 
-*/
