c72857236aa8fd7ead649867019d36d2d2927645
Aecio Silva <aeciofcs@gmail.com>
2020-07-09 20:39:05 -0300

    Foodfy: Finalizando criação de nova receita.

--- Foodfy/controllers/admin.js
+++ Foodfy/controllers/admin.js
@@ -1,4 +1,5 @@
 const data = require('../data.json')
+const fs   = require('fs')
 
 exports.index = (request, response) => {
     return response.render('Admin/index', { recipes: data.recipes })
@@ -8,8 +9,27 @@ exports.create = (request, response) => {
     return response.render('Admin/create')
 }
 
-exports.post = (request, response) => {
-    return response.send('GRAVAÇÃO do novo Cadastro da nova receita')
+exports.post = (request, response) => {    
+    const keys = Object.keys(request.body)        
+    for (const key of keys) {
+        if( request.body[key] == "")
+            return response.send('Por favor, preencha todos os campos.')
+    }
+    
+    let {imagem_url, nome_receita, autor_receita, informacoes_adicionais, ingrediente, modo_preparo} = request.body
+    data.recipes.push({
+        image: imagem_url,
+        title: nome_receita,
+        author: autor_receita,
+        ingredients: ingrediente,
+        preparation: modo_preparo,
+        information: informacoes_adicionais
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), (err)=>{
+        if (err) return request.send("Erro na gravação do arquivo DATA.JSON")
+        return response.redirect('/admin/recipes')
+    })
 }
 
 exports.show = (request, response) => {
--- Foodfy/public/scripts/scripts.js
+++ Foodfy/public/scripts/scripts.js
@@ -1,9 +1,6 @@
 const recipes       = document.querySelectorAll('.recipe');
 const esconderSpans = document.querySelectorAll('span');
 const listagem      = document.querySelectorAll('.lista-ingredientes')
-//const modalOverlay  = document.querySelector('.modal-overlay');
-//const modal         = document.querySelector('.modal');
-//const btnCloseModal = document.querySelector('.close-modal');
 
 // => Ouvir o clique de todas as receitas da pagina
 
@@ -12,60 +9,54 @@ for (const recipe of recipes) {
     recipe.addEventListener('click', ()=>{
         const title    = recipe.getAttribute('id');
         window.location.href = `/recipes/${title}`
-        //console.log(title)
-        //console.log(`/courses/${title}`)
-        
-        //modalOverlay.classList.toggle('active');
-        //modal.querySelector('img').src = `./assets/${imgId}.png`;
-        //modal.querySelector('h3').innerHTML = recipe.querySelector('h3').innerHTML;
-        //modal.querySelector('h4').innerHTML = recipe.querySelector('h4').innerHTML;        
     });
 }
 
-/*
-// Fechar o Modal Overlay 
-btnCloseModal.addEventListener('click', ()=>{
-    modalOverlay.classList.toggle('active');
-});
-
-// <= Ouvir o clique de todas as receitas da pagina
-*/
-
-/* => Ouvindo todos os cliques no label ESCONDER */
-//console.log(listagem.classList.contains('invisible'))
-
-//console.log(listagem[0].parentElement.className)
-//console.log(listagem[1].parentElement.className)
-//console.log(listagem[2].parentElement.className)
-for (const esconder of esconderSpans) {    
-    esconder.addEventListener( 'click', () => {        
+for (const esconder of esconderSpans) {
+    esconder.addEventListener( 'click', () => {
         for (const lista of listagem) {
-            //console.log(esconder.parentElement.firstElementChild.innerHTML.substr(1,5))
-            //console.log(esconder.parentElement.firstElementChild.innerHTML.substr(8,4))
-               // ==> console.log(lista.parentElement.className)
-               // => console.log(esconder.parentElement.firstElementChild.innerHTML)
-            //console.log(lista.parentElement.className.includes(esconder.parentElement.firstElementChild.innerHTML.substr(8,3)))            
-            //console.log(esconder.parentElement.firstElementChild.innerHTML.substr(8,3))
-            //console.log(lista.parentElement.getAttribute('id'))
-            //console.log(esconder.getAttribute('id'))
-            //if(esconder.parentElement.firstElementChild.innerHTML == lista.parentElement.className){
-            //if(lista.parentElement.className.includes(esconder.parentElement.firstElementChild.innerHTML.substr(9,2))){
             if(lista.parentElement.getAttribute('id') === esconder.getAttribute('id')){
                 lista.classList.toggle('invisible')
-                //esconder.innerText = 'MOSTRAR'                
             }
         }
     } )
 }
 
-/*
-for (const esconder of esconderSpans) {
-    esconder.addEventListener('click', () => {
-        console.log(esconder.innerText)
-        if (esconder.innerText == 'MOSTRAR'){
-            //listagem.classList.toggle('invisible')
-            esconder.innerText = 'ESCONDER'
-        }
-    })    
-}*/
-/* <= Ouvindo todos os cliques no label ESCONDER */
+/* CAMPO DINAMICO DA CRIACAO DE RECEITAS, ADMIN */
+function addIngrediente() {
+    const ingredients = document.querySelector("#ingredientes");
+    const fieldContainer = document.querySelectorAll(".ingrediente");
+  
+    // Realiza um clone do último ingrediente adicionado
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+  
+    // Não adiciona um novo input se o último tem um valor vazio
+    if (newField.children[0].value == "") return false;
+  
+    // Deixa o valor do input vazio
+    newField.children[0].value = "";
+    ingredients.appendChild(newField);
+}
+  
+document
+    .querySelector(".add-ingrediente")
+    .addEventListener("click", addIngrediente);
+
+function addMododePreparo() {
+    const preparos = document.querySelector("#preparos");
+    const fieldContainer = document.querySelectorAll(".preparo");
+    
+    // Realiza um clone do último ingrediente adicionado
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+    
+    // Não adiciona um novo input se o último tem um valor vazio
+    if (newField.children[0].value == "") return false;
+    
+    // Deixa o valor do input vazio
+    newField.children[0].value = "";
+    preparos.appendChild(newField);
+}
+    
+document
+    .querySelector(".add-preparo")
+    .addEventListener("click", addMododePreparo);
