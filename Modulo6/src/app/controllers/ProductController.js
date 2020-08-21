8c26c756f463e6a61b1ef1321448738de120e814
Aecio Silva <aeciofcs@gmail.com>
2020-08-21 14:37:43 -0300

    Modulo6: Conhecendo o asyn-await. Cadatro Produto

--- Modulo6/src/app/controllers/ProductController.js
+++ Modulo6/src/app/controllers/ProductController.js
@@ -1,4 +1,5 @@
 const Category = require('../models/Category')
+const Product  = require('../models/Product')
 
 module.exports = {
     create (request, response) {
@@ -8,14 +9,28 @@ module.exports = {
 
             const Categories = Results.rows
             return response.render('products/create.njk', {Categories})
-            
+
         }).catch((err) => {
             throw new Error(err)
         })
     },
 
-    post(request, response) {
+    async post(request, response) {
         // Lógica para Salvar o Produto
+        const keys = Object.keys(request.body)
+        for (key of keys) {
+            if(request.body[key] == ""){
+                return response.send('Todos os campos são Obrigatórios.')
+            }
+        }
+
+        let results      = await Product.create(request.body)
+        const productId  = results.rows[0].id
+        
+        results          = await Category.all()
+        const categories = results.rows
+
+        return response.render('products/create.njk', { productId, Categories: categories })
     }
 
 }
