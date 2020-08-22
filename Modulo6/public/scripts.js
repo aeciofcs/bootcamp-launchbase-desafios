19739fbe2808dcde6c540ce16e33387df1b812e2
Aecio Silva <aeciofcs@gmail.com>
2020-08-21 21:59:37 -0300

    Modulo6: Formatando em moeda (BR).

--- Modulo6/public/scripts.js
+++ Modulo6/public/scripts.js
@@ -3,7 +3,11 @@ input.addEventListener( "keydown", (event) => {
     setTimeout( () => {
         let { value } = event.target
         value = value.replace(/\D/g, "")
+        value = new Intl.NumberFormat('pt-BR', {
+            style: 'currency', 
+            currency: 'BRL'
+        }).format(value/100)
         event.target.value = value
     }, 1 )
-    
+
 } )
