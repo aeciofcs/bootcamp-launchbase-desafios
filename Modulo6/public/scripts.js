767a4d57d956edd48d38106621873be5799b426f
Aecio Silva <aeciofcs@gmail.com>
2020-08-23 00:04:09 -0300

    Modulo7: Refatorando o gerenciador de upload img.

--- Modulo6/public/scripts.js
+++ Modulo6/public/scripts.js
@@ -24,32 +24,43 @@ if (formDelete){
 }
 
 const PhotosUpload = {
+    preview: document.querySelector('#photos-preview'),
     uploadLimit: 6,
     handleFileInput(event){
-        const { files: fileList } = event.target
-        const { uploadLimit }     = PhotosUpload
-        if (fileList.length > uploadLimit){
-            alert(`Envie no máximo ${uploadLimit} fotos.`)
-            event.preventDefault()
-            return
-        }
+                
+        if (PhotosUpload.hasLimit(event)) return
 
         Array.from(fileList).forEach( file => {
             const reader = new FileReader()
             reader.onload = () => {
                 const image = new Image()
                 image.src = String(reader.result)
-
-                const div = document.createElement('div')
-                div.classList.add('photo')
-                div.onclick = () => alert('remover foto')
-                div.appendChild(image)
-                document.querySelector('#photos-preview').appendChild(div)
-
+                const div = PhotosUpload.getContainer(image)
+                PhotosUpload.preview.appendChild(div)
             }
 
             reader.readAsDataURL(file)
 
         } )
+    },
+    hasLimit(event){
+        const { files: fileList } = event.target
+        const { uploadLimit }     = PhotosUpload
+
+        if (fileList.length > uploadLimit){
+            alert(`Envie no máximo ${uploadLimit} fotos.`)
+            event.preventDefault()
+            return true
+        }
+        return false
+    },
+    getContainer(image){
+        const div = document.createElement('div')
+
+        div.classList.add('photo')
+        div.onclick = () => alert('remover foto')
+        div.appendChild(image)
+
+        return div
     }
 }
