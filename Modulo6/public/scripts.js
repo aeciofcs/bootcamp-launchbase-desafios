239a8706a70232625050ce6919f376c3ab6a2b36
Aecio Silva <aeciofcs@gmail.com>
2020-08-22 23:33:53 -0300

    Modulo7: Estrutura de dados do upload das imagens.

--- Modulo6/public/scripts.js
+++ Modulo6/public/scripts.js
@@ -22,3 +22,16 @@ if (formDelete){
         if(!confirmation) event.preventDefault()
     })
 }
+
+const PhotosUpload = {
+    uploadLimit: 6,
+    handleFileInput(event){
+        const { files: fileList } = event.target
+        const { uploadLimit }     = PhotosUpload
+        if (fileList.length > uploadLimit){
+            alert(`Envie no máximo ${uploadLimit} fotos.`)
+            event.preventDefault()
+            return
+        }
+    }
+}
